bc66846e1c8026eb361f41a1224c0b04fdf9835c
Matheus Timbó <matheusfild4@hotmail.com>
2019-11-18 08:13:16 -0300

    Adicionando loading pro login e mostrando alerta pra email não verificado

--- SHOW DO MILHAO/app/src/main/java/br/lucas/appshowdomilho/Login.kt
+++ SHOW DO MILHAO/app/src/main/java/br/lucas/appshowdomilho/Login.kt
@@ -3,11 +3,19 @@ package br.lucas.appshowdomilho
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
+import android.content.DialogInterface
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class Login : AppCompatActivity() {
 
@@ -19,11 +27,15 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loadingIndicator.visibility = View.INVISIBLE
         mAuth = FirebaseAuth.getInstance()
         mDatabase = FirebaseDatabase.getInstance()
         mDatabaseReference = FirebaseDatabase.getInstance().reference
 
         loginButton.setOnClickListener{
+
+            loadingIndicator.visibility = View.VISIBLE
+
             val email = emailTextInput.text.toString()
             val senha = senhaTextInput.text.toString()
 
@@ -32,6 +44,7 @@ class Login : AppCompatActivity() {
 
             mAuth!!.signInWithEmailAndPassword(email, senha).addOnCompleteListener{task->
                 if(task.isSuccessful){
+                    loadingIndicator.visibility = View.INVISIBLE
                     val user = mAuth!!.getCurrentUser()
                     if(user!!.isEmailVerified){
                         Toast.makeText(this,
@@ -44,6 +57,7 @@ class Login : AppCompatActivity() {
                     }
 
                 }else{
+                    loadingIndicator.visibility = View.INVISIBLE
                     Toast.makeText(this,
                         "Erro na tentativa de login",
                         Toast.LENGTH_SHORT).show()
@@ -68,6 +82,20 @@ class Login : AppCompatActivity() {
                         "Verifique seu email, email reenviado para " + mUser.getEmail(),
                         Toast.LENGTH_SHORT).show()
                     mAuth!!.signOut()
+
+                    AlertDialog.Builder(this)
+                        .setTitle("Verificar email")
+                        .setMessage("Você ainda não verificou seu email. Um novo email de verificação foi enviado. Caso não encontre, confira a caixa de spam")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(
+                            android.R.string.yes
+                        ) { dialog, which ->
+                            // Continue with delete operation
+                        }
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show()
                 } else {
                     Toast.makeText(this,
                         "Falha no reenvio da verificação de email",
